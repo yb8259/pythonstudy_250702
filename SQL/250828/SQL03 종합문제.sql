6c7aa99e2da0cd5ac8f74aba955c14e37e15729a
yb8259 <yb8259@naver.com>
2025-08-29 17:17:22 +0900

    250829

--- SQL/250828/SQL03 종합문제.sql
+++ SQL/250828/SQL03 종합문제.sql
@@ -32,24 +32,80 @@ FROM film;
 # 정렬후 행 번호를 매겨주세요.
 # customer_id, first_name, last_name, active, active_group
 
-WITH a AS (
-SELECT
-	customer_id,
-    first_name, last_name, active
-FROM customer
-ORDER BY active DESC
-),
-active_group as(
+WITH RankedCustomers AS (
 	SELECT
 		customer_id,
 		first_name, last_name, active,
-		NTILE(3) OVER () three
-FROM a
+		NTILE(3) OVER (ORDER BY active DESC) active_group
+	FROM customer
 )
+SELECT
+	customer_id, first_name, last_name, active, active_group,
+    ROW_NUMBER() OVER (PARTITION BY active_group ORDER BY customer_id) 
+						group_row_number
+FROM RankedCustomers;
+#---------------------------
+# 5. 영화 대여 내역에서 고객별 대여순서 출력, 이전 대여와의 간격 (day 단위 기준) 정보 출력,
+# 첫번째 대여일시 출력! 위 3가지를 포함한 내용을 출력해주세요.
+# customer_id, rental_id, rental_date, rental_order, prev_rental_gap, first_rental_date
 
+SELECT 
+	customer_id, rental_id, rental_date,
+    ROW_NUMBER() OVER(PARTITION BY customer_id ORDER BY rental_date) rental_order,
+    TIMESTAMPDIFF(DAY, LAG(rental_date, 1, 0) OVER(PARTITION BY customer_id # 디폴트는 바로 한번 직전, 2-3번째값은 옵션값
+							ORDER BY rental_date), rental_date) prev_rental_gap,
+	#DATEDIFF(rental_date, LAG(rental_date, 1, 0) 
+								#OVER(PARTITION BY customer_id) prev_rental_gap
+	FIRST_VALUE(rental_date) OVER(PARTITION BY customer_id ORDER BY rental_date) first_rental_date
+FROM rental;
+#---------------------------
+# 6. 각 고객의 결제 금액에 따른 순위 (결제 금액이 높은 순으로 정렬, 만약 동일한 값이 존재하는 경우
+# 같은 순위를 부여하지만, 다음 순위는 건너뛰지 않는다)를 출력해주시고, 백분위순위(결제금액이 높은 순으로 정렬) 출력alter
+# 2개 출력!
+WITH customer_amount AS(
 SELECT
 	customer_id,
-	first_name, last_name, active,
-    ROW_NUMBER () OVER(ORDER BY active)
-FROM active_group
-ORDER BY customer_id DESC;
+    SUM(amount) totaL_amount
+FROM payment
+GROUP BY customer_id)
+SELECT
+	customer_id, total_amount,
+	SUM(total_amount) OVER (ORDER BY total_amount DESC) final_total_amount,
+DENSE_RANK() OVER (ORDER BY total_amount) rank_dense,
+PERCENT_RANK() OVER (ORDER BY total_amount DESC) rank_percent
+FROM customer_amount;
+#---------------------------
+# 7. 각 등급별로 영화를 대여기간에 따라 4개의 그룹으로 나누고, 각 그룹 내에서
+# rental_duration이 낮은거에서 높은 순으로 번호를 매겨서 영화를 출력해주세요.
+# film_id, title, rating, rental_duration, rental_duration_group, group_row_number
+WITH FilmGroups AS (
+SELECT
+	film_id, title, rating, rental_duration,
+    NTILE(4) OVER (PARTITION BY rating ORDER BY rental_duration) rental_duration_group
+FROM film
+)
+SELECT
+	film_id, title, rating, rental_duration,
+    ROW_NUMBER() OVER(PARTITION BY rental_duration_group ORDER BY rental_duration) group_row_number
+FROM FilmGroups;
+#---------------------------
+# 8. 각 배우의 출연 영화 수에 따른 누적 분포를 다음정보와 함께 출력해주세요
+# actor_id, first_name, last_name, film_count, film_count_cume_dist
+WITH ActorFilm AS(
+SELECT 
+	A.actor_id,
+    CONCAT(A.first_name, " ", A.last_name) actor_name,
+    COUNT(*) film_count
+FROM actor A
+JOIN film_actor FA USING(actor_id)
+JOIN film F USING(film_id)
+GROUP BY A.actor_id)
+
+SELECT
+	actor_id,
+    actor_name,
+	film_count,
+	CUME_DIST() OVER (ORDER BY film_count) film_count_cume_dist
+FROM ActorFilm;
+#---------------------------
+# 9. 
--- SQL/250828/SQL04 (8th).sql
+++ SQL/250828/SQL04 (8th).sql
@@ -7,8 +7,8 @@
 WITH customer_rental AS(
 SELECT
 	customer_id, rental_date,
-	LAG(rental_date, 1, 0) OVER(PARTITION BY customer_id ORDER BY rental_date) before_rental_date, 
-	LEAD(rental_date, 1, 0) OVER(PARTITION BY customer_id ORDER BY rental_date) after_rental_date
+	LAG(rental_date) OVER(PARTITION BY customer_id ORDER BY rental_date) before_rental_date, 
+	LEAD(rental_date) OVER(PARTITION BY customer_id ORDER BY rental_date) after_rental_date
 FROM rental
 )
 SELECT 
@@ -24,4 +24,57 @@ SELECT
     CUME_DIST() OVER(PARTITION BY customer_id ORDER BY rental_date) cumulative_distribution,
     NTILE(3) OVER (PARTITION BY customer_id ORDER BY rental_date ASC) rental_group
 FROM customer_rental;
+
+#---------------------------
+# 선생님 풀이
+# 문제1. sakila DB의 “영화 대여 내역”을 바탕으로 다음 항목을 모두 출력하는 SQL 쿼리문을 작성해주세요.
+# 고객별 대여 순위, 이전 대여와의 간격, 다음 대여와의 간격,
+# 고객별 첫 번째 및 마지막 대여 일자, 고객별 대여 건의 백분위 순위 및 누적분포, 
+# 고객별 대여 내역의 3개 그룹 분할, 분할된 그룹 내 대여날짜 기준 오름차순 정렬
+# 위 항목들을 customer_id, rental_date와 함께 “모두 포함하여 출력”하는 SQL 쿼리를 작성해주세요.
+
+WITH rental_data AS (
+SELECT
+	rental_id, customer_id, rental_date,
+    ROW_NUMBER() OVER (PARTITION BY customer_id ORDER BY rental_date) rental_rank,
+    LAG(rental_date) OVER(PARTITION BY customer_id ORDER BY rental_date) previous_rental_date,
+	LEAD(rental_date) OVER(PARTITION BY customer_id ORDER BY rental_date) next_rental_date,
+    FIRST_VALUE(rental_date) OVER(PARTITION BY customer_id ORDER BY rental_date) first_rental_date,
+	LAST_VALUE(rental_date) OVER(PARTITION BY customer_id ORDER BY rental_date
+								ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING
+                                ) last_rental_date, # 해당 첫 스타트에서 마지막 까지 
+	PERCENT_RANK() OVER(PARTITION BY customer_id ORDER BY rental_date) rental_percentile_rank,
+    CUME_DIST() OVER(PARTITION BY customer_id ORDER BY rental_date) rental_cumulative_dist,
+    NTILE(3) OVER (PARTITION BY customer_id ORDER BY rental_date ASC) rental_group
+FROM rental
+),
+rental_intervals AS(
+	SELECT
+		rental_id, customer_id, rental_date, rental_rank,
+		DATEDIFF(rental_date, previous_rental_date)previous_rental_gap,
+		DATEDIFF(next_rental_date, rental_date)next_rental_gap,
+		first_rental_date, last_rental_date, rental_percentile_rank,
+		rental_cumulative_dist,rental_group
+	FROM rental_data
+),
+grouped_rental_rank AS (
+	SELECT
+	rental_id, customer_id, rental_date, rental_group,
+	ROW_NUMBER() OVER(PARTITION BY customer_id, rental_group ORDER BY rental_date) AS grouped_rental_rank 
+	FROM rental_data
+)
+SELECT
+ R.customer_id,
+ R.rental_date,
+ R.rental_rank,
+ R.previous_rental_gap,
+ R.next_rental_gap,
+ R.first_rental_date,
+ R.last_rental_date,
+ R.rental_percentile_rank,
+ R.rental_cumulative_dist,
+ R.rental_group,
+ G.grouped_rental_rank
+FROM rental_intervals R
+JOIN grouped_rental_rank G USING(rental_id);
 
